4c9601471f7838ecf37c41fde31002c22aec559e
ak78887 <ankurmaurya286@gmail.com>
2023-05-11 12:32:25 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/gskinner/constellations_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gskinner.constellations_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/gskinner/star_field/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gskinner.star_field
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
